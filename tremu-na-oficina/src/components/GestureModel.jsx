56046d570933003bababacc2d9b827f202acc9b3
Isabelly Hartmann <a14844@oficina.pt>
2026-06-12 11:51:39 +0000

    atualizar

--- tremu-na-oficina/src/components/GestureModel.jsx
+++ tremu-na-oficina/src/components/GestureModel.jsx
@@ -20,7 +20,8 @@ export default function GestureModel({ letraAlvo, onLetraIdentificada }) {
 
                 const instancia = await HandLandmarker.createFromOptions(vision, {
                     baseOptions: {
-                        modelAssetPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.35/hand_landmarker.task',
+                        //modelAssetPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.35/hand_landmarker.task',
+                        modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task',
                         delegate: 'CPU'
                     },
                     runningMode: 'VIDEO',
